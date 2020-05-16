147b1372073f85e476294417d010f9c45f439c5f
Daniel Aguilar <daguilar@thoughtworks.com>
2020-05-16 18:05:22 -0400

    Adds constants

--- src/data/clients/BreedingApiClient.js
+++ src/data/clients/BreedingApiClient.js
@@ -1,12 +1,13 @@
+import {DOG_API_LIST_ALL_URL, DOG_API_BASE_URL} from "../constants/Endpoints";
 class BreedingApiClient {
     async findAll() {
-        const response = await fetch("https://dog.ceo/api/breeds/list/all");
+        const response = await fetch(DOG_API_LIST_ALL_URL);
         const data = await response.json();
         return data;
     }
 
     async findImageByBreed(breedName) {
-        const response = await fetch(`https://dog.ceo/api/breed/${breedName}/images/random`);
+        const response = await fetch(`${DOG_API_BASE_URL}/breed/${breedName}/images/random`);
         const data = await response.json();
         return data;
     }
---
+++ src/data/constants/Endpoints.js
@@ -0,0 +1,2 @@
+export const DOG_API_BASE_URL = "https://dog.ceo/api";
+export const DOG_API_LIST_ALL_URL = `${DOG_API_BASE_URL}/breeds/list/all`;
